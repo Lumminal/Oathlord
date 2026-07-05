Trauma-Station/Trauma-Station

trauma mod (#11)
* trauma mod

---------

Co-authored-by: deltanedas <@deltanedas:kde.org>

runclient-Tools.bat
@@ -5,4 +5,4 @@ REM
 REM SPDX-License-Identifier: AGPL-3.0-or-later
 
 @echo off
-dotnet run --project Content.Goobstation.Client --configuration Tools
+dotnet run --project Content.Trauma.Client --configuration Tools

runclient-Tools.sh
@@ -6,5 +6,5 @@
 # SPDX-License-Identifier: AGPL-3.0-or-later
 
 #!/bin/sh
-dotnet run --project Content.Goobstation.Client --configuration Tools
+dotnet run --project Content.Trauma.Client --configuration Tools
 read -p "Press enter to continue"

runclient.bat
@@ -8,4 +8,4 @@ REM
 REM SPDX-License-Identifier: AGPL-3.0-or-later
 
 @echo off
-dotnet run --project Content.Goobstation.Client
+dotnet run --project Content.Trauma.Client

runclient.sh
@@ -4,5 +4,5 @@
 # SPDX-License-Identifier: AGPL-3.0-or-later
 
 #!/bin/sh
-dotnet run --project Content.Goobstation.Client
+dotnet run --project Content.Trauma.Client
 read -p "Press enter to continue"

runserver-Tools.bat
@@ -5,5 +5,5 @@ REM
 REM SPDX-License-Identifier: AGPL-3.0-or-later
 
 @echo off
-dotnet run --project Content.Goobstation.Server --configuration Tools
+dotnet run --project Content.Trauma.Server --configuration Tools
 pause

runserver-Tools.sh
@@ -6,5 +6,5 @@
 # SPDX-License-Identifier: AGPL-3.0-or-later
 
 #!/bin/sh
-dotnet run --project Content.Goobstation.Server --configuration Tools
+dotnet run --project Content.Trauma.Server --configuration Tools
 read -p "Press enter to continue"

runserver.bat
@@ -8,5 +8,5 @@ REM
 REM SPDX-License-Identifier: AGPL-3.0-or-later
 
 @echo off
-dotnet run --project Content.Goobstation.Server
+dotnet run --project Content.Trauma.Server
 pause

runserver.sh
@@ -4,5 +4,5 @@
 # SPDX-License-Identifier: AGPL-3.0-or-later
 
 #!/bin/sh
-dotnet run --project Content.Goobstation.Server
+dotnet run --project Content.Trauma.Server
 read -p "Press enter to continue"

Lumminal/Oathlord

runclient-Tools.bat
@@ -1,3 +1,2 @@
 @echo off
-dotnet run --project Content.Client --configuration Tools
-pause
+dotnet run --project Content.Oathlord.Client --configuration Tools

runclient-Tools.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-dotnet run --project Content.Client --configuration Tools
+dotnet run --project Content.Oathlord.Client --configuration Tools
 read -p "Press enter to continue"

runclient.bat
@@ -1,3 +1,2 @@
 @echo off
-dotnet run --project Content.Client
-pause
+dotnet run --project Content.Oathlord.Client

runclient.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-dotnet run --project Content.Client
+dotnet run --project Content.Oathlord.Client
 read -p "Press enter to continue"

runserver-Tools.bat
@@ -1,3 +1,3 @@
 @echo off
-dotnet run --project Content.Server --configuration Tools
+dotnet run --project Content.Oathlord.Server --configuration Tools
 pause

runserver-Tools.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-dotnet run --project Content.Server --configuration Tools
+dotnet run --project Content.Oathlord.Server --configuration Tools
 read -p "Press enter to continue"

runserver.bat
@@ -1,3 +1,3 @@
 @echo off
-dotnet run --project Content.Server
+dotnet run --project Content.Oathlord.Server
 pause

runserver.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-dotnet run --project Content.Server
+dotnet run --project Content.Oathlord.Server
 read -p "Press enter to continue"