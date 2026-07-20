fda1e373a76167434cdc8ae6f7a95d2a46bde9fd
Lumminal <spotifylefteris@gmail.com>
2026-07-20 17:48:47 +0300

    init

---
+++ Content.Oathlord.Shared/Economy/Components/EconomyAccountComponent.cs
@@ -0,0 +1,17 @@
+﻿using Robust.Shared.GameStates;
+
+namespace Content.Oathlord.Shared.Economy.Components;
+
+/// <summary>
+/// Component that is applied to the player once they spawn.
+/// Allows them to be part of the economy, by initializing a new bank account in the economy system.
+/// </summary>
+[RegisterComponent, NetworkedComponent]
+public sealed partial class EconomyAccountComponent : Component
+{
+    /// <summary>
+    /// The current amount of currency this entity has stored in the bank.
+    /// </summary>
+    [DataField]
+    public int Stored;
+}
---
+++ Content.Oathlord.Shared/Economy/Components/EconomyMapComponent.cs
@@ -0,0 +1,17 @@
+﻿using Robust.Shared.GameStates;
+
+namespace Content.Oathlord.Shared.Economy.Components;
+
+/// <summary>
+/// Component applied to the map to store information about accounts, and anything related to the economy.
+/// </summary>
+[RegisterComponent, NetworkedComponent]
+[AutoGenerateComponentState]
+public sealed partial class EconomyMapComponent : Component
+{
+    /// <summary>
+    /// All the active accounts in the economy
+    /// </summary>
+    [DataField, AutoNetworkedField]
+    public HashSet<EntityUid> ActiveAccounts = new();
+}
