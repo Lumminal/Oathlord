8cdce4f248cfaef4d04a56f4638c870d2fb0dc54
Lumminal <spotifylefteris@gmail.com>
2026-07-23 21:08:58 +0300

    (add) deposit, documentation + other - Added deposit window for economy machine - Added documentation to some parts of the code - Added some new api methods

--- Content.Oathlord.Shared/Economy/Components/EconomyMachineComponent.cs
+++ Content.Oathlord.Shared/Economy/Components/EconomyMachineComponent.cs
@@ -1,13 +1,35 @@
-﻿using Robust.Shared.Serialization;
+﻿using Robust.Shared.GameStates;
+using Robust.Shared.Serialization;
 
 namespace Content.Oathlord.Shared.Economy.Components;
 
 /// <summary>
 /// Component that is used for objects, to allow withdrawing and depositing money via UI
 /// </summary>
+[RegisterComponent, NetworkedComponent]
 public sealed partial class EconomyMachineComponent : Component
 {
 
+}
+
+[Serializable, NetSerializable]
+public sealed class EconomyDepositMessage : BoundUserInterfaceMessage
+{
+    /// <summary>
+    /// The entity the amount is getting deposited to
+    /// </summary>
+    public NetEntity? DepositEntity;
+
+    /// <summary>
+    /// The amount to deposit to the entity
+    /// </summary>
+    public int Amount;
+
+    public EconomyDepositMessage(NetEntity? depositEntity, int amount)
+    {
+        DepositEntity = depositEntity;
+        Amount = amount;
+    }
 }
 
 [Serializable, NetSerializable]
