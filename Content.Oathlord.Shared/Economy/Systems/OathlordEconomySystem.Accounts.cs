54719d73a51ca71cff762a1653c3aabe7d5a6a27
Lumminal <spotifylefteris@gmail.com>
2026-07-25 00:37:35 +0300

    (add) ulra-simple withdraw - Copy-pasted from depositing, but needs to support outputting nar

--- Content.Oathlord.Shared/Economy/Systems/OathlordEconomySystem.Accounts.cs
+++ Content.Oathlord.Shared/Economy/Systems/OathlordEconomySystem.Accounts.cs
@@ -9,15 +9,30 @@ public sealed partial class OathlordEconomySystem
 {
     /// <summary>
     /// Adds a specified amount of currency (Nar) to the account
-    /// This can be also negative.
-    /// TODO: if negative it gets added to debt
+    /// TODO: if negative, and stored is 0 then it should get added to debt variable
     /// </summary>
-    public void AddCurrencyToAccount(Entity<EconomyAccountComponent?> ent, int amount)
+    /// <param name="ent">The account to withdraw from</param>
+    /// <param name="amount">The amount to adjust</param>
+    public void AdjustCurrencyFromAccount(Entity<EconomyAccountComponent?> ent, int amount)
     {
         if (!_econAccountQuery.Resolve(ent.Owner, ref ent.Comp))
             return;
 
         ent.Comp.Stored += amount;
+        Dirty(ent);
+    }
+
+    /// <summary>
+    /// Withdraws a specified amount of currency from the account.
+    /// </summary>
+    /// <param name="ent">The account to withdraw from</param>
+    /// <param name="amount">The amount to withdraw, make sure its positive as negative values or zero won't work</param>
+    public void WithdrawFromAccount(Entity<EconomyAccountComponent?> ent, int amount)
+    {
+        if (!_econAccountQuery.Resolve(ent.Owner, ref ent.Comp) || amount <= 0)
+            return;
+
+        ent.Comp.Stored = Math.Clamp(ent.Comp.Stored - amount, 0, int.MaxValue); // TODO: There should be a cap instead of int.MaxValue
         Dirty(ent);
     }
 }
