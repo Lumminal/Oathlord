32c4658c0bc6f8a3f253c78bdbaf09722f632791
Lumminal <spotifylefteris@gmail.com>
2026-07-29 22:09:21 +0300

    ddd

--- Content.IntegrationTests/Tests/PostMapInitTest.cs
+++ Content.IntegrationTests/Tests/PostMapInitTest.cs
@@ -103,7 +103,6 @@ namespace Content.IntegrationTests.Tests
         [Test, TestCaseSource(nameof(Grids))]
         [Ignore("Oathlord")]
         [EnsureCVar(Side.Server, typeof(CCVars), nameof(CCVars.GridFill), false)]
-        [Ignore("Oathlord")]
         public async Task GridsLoadableTest(string mapFile)
         {
             var pair = Pair;
@@ -138,7 +137,6 @@ namespace Content.IntegrationTests.Tests
         [TestCaseSource(nameof(ShuttleMapFiles))]
         [Ignore("Oathlord")]
         [EnsureCVar(Side.Server, typeof(CCVars), nameof(CCVars.GridFill), false)]
-        [Ignore("Oathlord")]
         public async Task ShuttlesLoadableTest(ResPath path)
         {
             var pair = Pair;
@@ -329,7 +327,6 @@ namespace Content.IntegrationTests.Tests
         [Test, TestCaseSource(nameof(GameMaps))]
         [Ignore("Oathlord")]
         [EnsureCVar(Side.Server, typeof(CCVars), nameof(CCVars.GridFill), false)]
-        [Ignore("Oathlord")]
         public async Task GameMapsLoadableTest(string mapProto)
         {
             var pair = Pair;
@@ -473,7 +470,6 @@ namespace Content.IntegrationTests.Tests
         [TestCaseSource(nameof(AllMapFiles))]
         [Ignore("Oathlord")]
         [EnsureCVar(Side.Server, typeof(CCVars), nameof(CCVars.GridFill), false)]
-        [Ignore("Oathlord")]
         public async Task NonGameMapsLoadableTest(ResPath mapPath)
         {
             var pair = Pair;
